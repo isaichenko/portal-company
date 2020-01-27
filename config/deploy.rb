ba7e3f7cbffd5f54ff07ba0dc0d5e5624c550205
isaichenko <isaichenko.e@gmail.com>
2020-01-27 15:36:06 +0200

    capistrano updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, "/var/www/dolina-portal"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
