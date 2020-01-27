7fd0e143c40a4c66e063281390d497987fc44bb2
isaichenko <isaichenko.e@gmail.com>
2020-01-27 16:58:47 +0200

    rvm updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,4 +36,4 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-set :rvm_map_bins, %w{gem rake ruby rails bundle}
+#set :rvm_map_bins, %w{gem rake ruby rails bundle}
