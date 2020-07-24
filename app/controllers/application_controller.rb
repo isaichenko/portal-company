e75d78013763a90e0c96c5a2e3413537461eb43a
isaichenko <isaichenko.e@gmail.com>
2020-07-24 13:18:34 +0300

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,11 @@ class ApplicationController < ActionController::Base
     #if request.remote_addr !~ /^192.168.0.\d{1,3}$/ || request.remote_addr !~ /^31.128.72.125$/ || request.remote_addr !~ /^95.46.157.148$/
     #if request.remote_ip !~ /^192.168.0.\d{1,3}$/ || request.remote_ip != "31.128.72.125" || request.remote_ip != "95.46.157.148"
     if Rails.env == 'production'
-      if request.remote_ip != "31.128.72.125" || request.remote_ip != "95.46.157.148" || request.remote_ip !~ /^192.168.0.\d{1,3}$/
-        authenticate_or_request_with_http_basic 'Auth' do |name, password|
-          name == 'user' && password == 'Dolina2020'
+      if request.remote_ip !~ /^192.168.0.\d{1,3}$/
+        if request.remote_ip != "31.128.72.125" || request.remote_ip != "95.46.157.148"
+          authenticate_or_request_with_http_basic 'Auth' do |name, password|
+            name == 'user' && password == 'Dolina2020'
+          end
         end
       end
     end
