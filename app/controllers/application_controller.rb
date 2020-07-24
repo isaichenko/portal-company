d5810498af12018e4ea959378c5686ae10d3aee1
isaichenko <isaichenko.e@gmail.com>
2020-07-24 12:30:04 +0300

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_session
 
-  before_filter :authenticate_if_not_local
+  before_action :authenticate_if_not_local
 
   private
 
