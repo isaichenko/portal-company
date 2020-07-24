ac984c01b8433261624250576ed61ab0eb28fe46
isaichenko <isaichenko.e@gmail.com>
2020-07-24 16:04:35 +0300

    exc group

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,16 @@ Rails.application.routes.draw do
   get '/ip_phones', 		to: "employees#ip_phone", 		   as: :ip_phones
   get '/search',    		to: "employees#search",   		   as: :search_page
   get '/department_search', to: "employees#department_search", as: :department_search
+
+  #Static Pages
+  get '/koblevo', to: "static_pages#koblevo", as: :koblevo
+  get '/program', to: "static_pages#program", as: :program
+  get '/exc_group1', to: "static_pages#exc_group1", as: :exc_group1
+  get '/exc_group2', to: "static_pages#exc_group2", as: :exc_group2
+  get '/exc_group3', to: "static_pages#exc_group3", as: :exc_group3
+  get '/training1', to: "static_pages#training1", as: :training1
+  get '/training2', to: "static_pages#training2", as: :training2
+  get '/training3', to: "static_pages#training3", as: :training3
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "employees#index"
 end
