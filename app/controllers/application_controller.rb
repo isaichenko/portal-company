fc83810043fd4d59a0390375308145314dd8a505
isaichenko <isaichenko.e@gmail.com>
2020-07-24 12:45:55 +0300

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_if_not_local
+    puts "====================================================="
+    puts "Remote_addr #{request.remote_ip}"
+    puts "====================================================="
     #if Rails.env == 'production' && request.remote_addr !~ /^192.168.0.\d{1,3}$/
     if request.remote_addr !~ /^192.168.0.\d{1,3}$/ || request.remote_addr !~ /^31.128.72.125$/ || request.remote_addr !~ /^95.46.157.148$/
       authenticate_or_request_with_http_basic 'Production' do |name, password|
