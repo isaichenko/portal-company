22e21d225b79dc96550d12f79cf5b070ca9f0048
isaichenko <isaichenko.e@gmail.com>
2020-07-24 12:51:06 +0300

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_session
 
-  #before_action :authenticate_if_not_local
+  before_action :authenticate_if_not_local
 
   private
 
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
     puts "Remote_addr #{request.remote_ip}"
     puts "====================================================="
     #if Rails.env == 'production' && request.remote_addr !~ /^192.168.0.\d{1,3}$/
-    if request.remote_addr !~ /^192.168.0.\d{1,3}$/ || request.remote_addr !~ /^31.128.72.125$/ || request.remote_addr !~ /^95.46.157.148$/
+    #if request.remote_addr !~ /^192.168.0.\d{1,3}$/ || request.remote_addr !~ /^31.128.72.125$/ || request.remote_addr !~ /^95.46.157.148$/
+    if request.remote_ip !~ /^192.168.0.\d{1,3}$/ || request.remote_ip !~ /^31.128.72.125$/ || request.remote_ip !~ /^95.46.157.148$/
       authenticate_or_request_with_http_basic 'Production' do |name, password|
         name == 'user' && password == 'Dolina2020'
       end
