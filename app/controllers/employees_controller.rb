130cd8c6b9328e96a8b551d13e91218e3fec5c4b
isaichenko <isaichenko.e@gmail.com>
2020-08-12 18:02:34 +0300

    department updates

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -120,7 +120,7 @@ class EmployeesController < ApplicationController
                                       :birthday,
                                       :position,
                                       :avatar,
-                                      :department,
+                                      #:department,
                                       :department_id)
     end
 
